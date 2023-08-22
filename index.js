e122d84e10f0413a30335e1c6f18af75d511b371
Frandorr1 <142456182+Frandorr1@users.noreply.github.com>
2023-08-21 21:47:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+* GABRIEL
+* PRIMEIRO COMENTARIO
+*/
+console.log("olá mundo!");
+//promot("Franciele");
+
+//Variaveis
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelB);
+let numero = 50;
+console.log(numero);
+numero = 80
+console.log(numero);
+
+
+const nome = "Franciele";
+const idade = 28;
+
+console.log("ola!", "Meu nome é", nome, "e eu tenho", idade, "anos.");
+
+
+// Variaveis
+const variavelF = 1000;
+const nomeexercicio = "Fran";
+const sobrenomeexercicio = "Dorr";
+const idadeexercicio = 28;
+const estudante = "SIM.";
+
+console.log("olá!", "Meu nome é", nomeexercicio, "eu tenho", idadeexercicio, "anos.", "Status de estudante:", estudante);
+
+
